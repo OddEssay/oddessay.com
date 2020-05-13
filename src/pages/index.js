241973c058aae4be3afb67bda450dee31df6a730
Paul Bennett-Freeman <clicknmix@gmail.com>
2020-05-13 17:52:38 +0100

    Inital build from gatsby new

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="pf.is" />
+    <h1>Short Links by Paul Bennett-Freeman</h1>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+  </Layout>
+)
+
+export default IndexPage
